47857a94159de59e0bcf30b54d542730b5a4cf81
Gigimikava <127594709+Gigimikava@users.noreply.github.com>
2023-05-28 10:49:48 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -16,23 +16,22 @@ class books(db.Model):
     title = db.Column(db.String(50), nullable=False)
     price = db.Column(db.String(120), nullable=False)
 
-
     with app.app_context():
-        url = 'http://books.toscrape.com'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
 
-        books = []
-        for book in soup.find_all('article', class_='product_pod'):
-            title = book.h3.a['title']
-            price = book.find('p', class_='price_color').text
-            print(title, price)
-            time.sleep(2)
         db.create_all()
 
 @app.route('/')
 def home():
+    url = 'http://books.toscrape.com'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
+    books = []
+    for book in soup.find_all('article', class_='product_pod'):
+        title = book.h3.a['title']
+        price = book.find('p', class_='price_color').text
+        print(title, price)
+        time.sleep(2)
     return render_template('index.html', books=books)
 
 @app.route("/login", methods=['GET', 'POST'])
