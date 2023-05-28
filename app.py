dd11effce2773b0d0aa3cdad55ea0b7066610fe2
Gigimikava <127594709+Gigimikava@users.noreply.github.com>
2023-05-28 10:01:43 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,70 @@
-from flask import Flask, render_template, url_for, redirect, session, request, flash, get_flashed_messages
+from flask import Flask, render_template, url_for, redirect, session, request, flash, get_flashed_messages, request
+import requests
+from bs4 import BeautifulSoup
+import time
+# import csv
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Python'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.String(120), nullable=False)
+
+
+    with app.app_context():
+        url = 'http://books.toscrape.com'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        books = []
+        for book in soup.find_all('article', class_='product_pod'):
+            title = book.h3.a['title']
+            price = book.find('p', class_='price_color').text
+            print(title, price)
+            time.sleep(2)
+        db.create_all()
+
+@app.route('/')
+def home():
+
+    return render_template('index.html', books=books)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         session['username'] = request.form["username"]
-        return redirect(url_for("hello my new user"))
+        flash("login succesfully")
+        return redirect(url_for("/"))
     return render_template('login.html')
-class Books(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)
-    title = db.Column('title', db.String(30))
-    author = db.Column('author', db.String(40))
-    price = db.Column('price', db.Integer)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
+    def __repr__(self):
+        return f'<User {self.username}>'
+    with app.app_context():
+
+        db.create_all()
 
 @app.route("/books", methods=['GET', 'POST'])
-def Books():
+def books():
     if request.method == "POST":
         title = request.form["title"]
-        author = request.form["author"]
         price = request.form["price"]
-        book = Books(title=title, author=author, price=price)
+        book = books(title=title, price=price)
         db.session.add(book)
         db.session.commit()
         flash("Added Successfully! ")
-        return redirect(url_for("hello_world"))
+        return redirect(url_for("Books"))
     return render_template("books.html")
 
 @app.route ("/logout")
@@ -45,17 +80,16 @@ def search():
     return render_template('search.html')
 
 @app.route('/Registration',methods=['GET','POST'])
-def Register():
-    if request == 'post':
+def register():
+    if request.method == 'POST':
         firstname = request.form['firstname']
         lastname = request.form['lastname']
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        registration = Register(firstname=firstname,lastname=lastname,username=username,email=email,password=password)
-        db.session.add(registration)
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
         db.session.commit()
-        flash('Registration was successful')
         return redirect(url_for('register'))
     return render_template('registration.html')
 
